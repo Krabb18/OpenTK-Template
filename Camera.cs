b2a3f59194418f4b792b799538d3605194a4b649
Krabb <103280977+Krabb18@users.noreply.github.com>
2025-11-12 20:05:09 +0100

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,74 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SpaceEngine.Engine
+{
+    public class Camera
+    {
+        Matrix4 viewMatrix = Matrix4.Identity;
+        Matrix4 projectionMatrix = Matrix4.Identity;
+        Matrix4 camMatrix = Matrix4.Identity;
+
+        Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 target = new Vector3(0.0f, 0.0f, -1.0f);
+        float zoom = 1.0f;
+        Vector3 front = Vector3.Zero;
+        public float width = 0.0f;
+        public float height = 0.0f;
+
+        float speed = 0.5f;
+
+        public Camera()
+        {
+
+        }
+
+        public void CaculateMatrix()
+        {
+            //for testing
+            target = position + new Vector3(0.0f, 0.0f, -1.0f);
+            front = Vector3.Normalize(target - position);
+
+            viewMatrix = Matrix4.LookAt(position, target, new Vector3(0.0f, 1.0f, 0.0f));
+            projectionMatrix =  Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+            camMatrix =  viewMatrix * projectionMatrix;
+        }
+
+        public void Activate(ShaderClass shaderClass)
+        {
+            shaderClass.Activate();
+            int modelLoc = GL.GetUniformLocation(shaderClass.ID, "camMatrix");
+            GL.UniformMatrix4(modelLoc, false, ref camMatrix);
+        }
+
+        public void Movement(KeyboardState input, float dt)
+        {
+            if (input.IsKeyDown(Keys.W))
+            {
+                position += front * speed * dt;
+            }
+
+            if (input.IsKeyDown(Keys.S))
+            {
+                position -= front * speed * dt;
+            }
+
+            if (input.IsKeyDown(Keys.A))
+            {
+                position -= Vector3.Normalize(Vector3.Cross(front, new Vector3(0.0f, 1.0f, 0.0f))) * speed * dt;
+            }
+
+            if (input.IsKeyDown(Keys.D))
+            {
+                position += Vector3.Normalize(Vector3.Cross(front, new Vector3(0.0f, 1.0f, 0.0f))) * speed * dt;
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,77 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using SpaceEngine.Engine;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceEngine
+{
+    public class Game : GameWindow
+    {
+        TestObject testObject;
+        Camera cam;
+
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            cam = new Camera();
+
+            cam.width = 1280.0f;
+            cam.height = 720.0f;
+
+            Console.WriteLine(cam.width);
+            Console.WriteLine(cam.height);
+
+            testObject = new TestObject();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            
+            cam.Movement(KeyboardState, (float)e.Time);
+            cam.CaculateMatrix();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            cam.Activate(testObject.shader);
+            testObject.Render();
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+            cam.width = e.Width;
+            cam.height = e.Height;
+        }
+
+    }
+}
